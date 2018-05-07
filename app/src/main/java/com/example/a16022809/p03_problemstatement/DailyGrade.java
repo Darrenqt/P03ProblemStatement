7610a14ebd55489bb495b81cd901bb4c5cbc8a0e
16022809 <darren410ss@gmail.com>
2018-05-07 14:07:38 +0800

    UI for info_row

---
+++ app/src/main/java/com/example/a16022809/p03_problemstatement/DailyGrade.java
@@ -0,0 +1,20 @@
+package com.example.a16022809.p03_problemstatement;
+
+public class DailyGrade {
+    private String week;
+    private String grade;
+
+    public DailyGrade(String week, String grade) {
+        this.week = week;
+        this.grade = grade;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+}
