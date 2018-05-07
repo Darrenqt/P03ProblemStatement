b5cd96448e58f90cdf290d7524f7ee916d285212
16046512 <16046512@myrp.edu.sg>
2018-05-07 14:29:05 +0800

    Main Activity and info completed

--- app/src/main/java/com/example/a16022809/p03_problemstatement/Course.java
+++ app/src/main/java/com/example/a16022809/p03_problemstatement/Course.java
@@ -3,24 +3,30 @@ package com.example.a16022809.p03_problemstatement;
 public class Course {
     String moduleName;
     String moduleCode;
+    String link;
 
-    public Course(String moduleName, String moduleCode) {
+    public Course(String moduleName, String moduleCode,String link) {
         this.moduleName = moduleName;
         this.moduleCode = moduleCode;
+        this.link = link;
     }
 
+
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
     public String getModuleName() {
         return moduleName;
     }
-
-    public void setModule(String moduleName) {
+    public void setModuleName(String moduleName) {
         this.moduleName = moduleName;
     }
-
     public String getModuleCode() {
         return moduleCode;
     }
-
     public void setModuleCode(String moduleCode) {
         this.moduleCode = moduleCode;
     }
--- app/src/main/java/com/example/a16022809/p03_problemstatement/MainActivity.java
+++ app/src/main/java/com/example/a16022809/p03_problemstatement/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a16022809.p03_problemstatement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +14,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView lvModule;
-    private TextView moduleName,moduleCode;
     private ArrayAdapter aa;
     private ArrayList<Course> course;
 
@@ -24,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         lvModule = (ListView)findViewById(R.id.listviewModule);
         course = new ArrayList<Course>();
-        course.add(new Course("Android Programming II","C347"));
-        course.add(new Course("Web Services","C347"));
+        course.add(new Course("Android Programming II","C347","https://www.rp.edu.sg/schools-courses/courses/full-time-diplomas/full-time-courses/modules/index/C347"));
+        course.add(new Course("Web Services","C302","https://www.rp.edu.sg/schools-courses/courses/full-time-diplomas/full-time-courses/modules/index/C302"));
 
 
         aa = new CourseAdapter(this, R.layout.modulerow, course);
@@ -36,7 +36,11 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Course selectedcourse = course.get(i);
                 //toast
-                Toast.makeText(MainActivity.this,selectedcourse.getModuleCode()+":"+selectedcourse.moduleName,Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this,selectedcourse.getModuleCode()+":"+selectedcourse.getLink(),Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, Info.class);
+                intent.putExtra("courseCode",selectedcourse.getModuleCode());
+                intent.putExtra("link",selectedcourse.getLink());
+                startActivity(intent);
             }
         });
     }
