1ef4cfa216e0140d3812e9b6adf777dde9d3d8ae
14049472 <14049472@myrp.edu.sg>
2018-05-07 13:03:26 +0800

    Merge branch 'master' of C:\Android\workspace\P03ProblemStatement with conflicts.

---
+++ app/src/main/java/com/example/a16022809/p03_problemstatement/AddActivity.java
@@ -0,0 +1,13 @@
+package com.example.a16022809.p03_problemstatement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
